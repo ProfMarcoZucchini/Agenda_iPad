017d66f26c2442ed84bc444561d7db2d5761c829
Marco Zucchini <122208517+ProfMarcoZucchini@users.noreply.github.com>
2026-09-02 14:53:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'agenda-ipad-reintegration-';
-const CACHE = 'agenda-ipad-reintegration-0.1.60';
+const CACHE = 'agenda-ipad-reintegration-0.1.61';
 const CORE = ['./', './index.html', './src/main.js', './src/sync-core.js', './src/lan-sync.js', './src/cloud-sync.js', './src/cloud-crypto.js', './src/blob-store.js', './src/backup.js', './src/cloud-auth.js', './src/styles.css', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png', './apple-touch-icon.png', './assets/cover-agenda-ipad.png', './assets/welcome-agenda-ipad.png', './assets/weather/sun.svg', './assets/weather/sun-cloud.svg', './assets/weather/cloud.svg', './assets/weather/rain.svg', './assets/weather/fog.svg', './assets/weather/snow.svg'];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)).then(() => self.skipWaiting()));
